12e1175dff9d031e2acc8b055100f2aec69d3b3f
chica25 <pfco520@gmail.com>
2021-05-09 22:50:50 -0400

    Implement chatroom class and attributes

--- chatroom.js
+++ chatroom.js
@@ -2,3 +2,11 @@
 // setup a real-time listener to get the new chats
 // update the username
 // update the room
+
+class Chatroom {
+    constructor(room, username) {
+        this.room = room;
+        this.username;
+        this.chats = db.collection('chat');
+    }
+}
