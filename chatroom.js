cea8b73260aedc3249c6740ad1e811f3f3855f12
chica25 <pfco520@gmail.com>
2021-05-09 23:11:27 -0400

    test new chat to the database

--- chatroom.js
+++ chatroom.js
@@ -9,7 +9,24 @@ class Chatroom {
         this.username = username;
         this.chats = db.collection('chat');
     }
+    async newChat(message){
+        const now = new Date();
+        const chat = {
+            message,
+            username: this.username,
+            room: this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(now)
+        };
+        // save chat to database
+        const response = await this.chats.add(chat)
+        return response;
+    }
 }
 
+
 const chatroom = new Chatroom('general', 'pat');
-console.log(chatroom)
+// console.log(chatroom)
+
+chatroom.newChat('Hey')
+.then(() => console.log('chat added'))
+.catch(err => console.log(err));
